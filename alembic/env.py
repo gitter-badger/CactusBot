719d7234b9b7ed56697f026636557d8c5fffb0bb
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-04-10 21:09:03 -0400

    Make Travis happy

--- alembic/env.py
+++ alembic/env.py
@@ -2,8 +2,6 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-import sys
-from os.path import abspath, dirname
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -37,6 +35,8 @@ def run_migrations_offline():
     script output.
 
     """
+    alembic_config = config.get_section(config.config_ini_section)
+    db_path = alembic_config["here"] + "/data/data.db"
 
     context.configure(
         url=db_path, target_metadata=target_metadata, literal_binds=True)
